1f9b7e0a503fa7882969769d1f0d732252095d66
ashwink0 <ashwin.kuppahally@gmail.com>
2021-05-11 17:59:07 -0700

    Remove cors

--- src/pages/api/addFile/index.ts
+++ src/pages/api/addFile/index.ts
@@ -4,12 +4,8 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { v4 as uuid4 } from 'uuid';
 import { storage } from '../../../../lib/firebase-admin';
 import { withSentry } from '@sentry/nextjs';
-import Cors from 'cors'
 const stream = require(`stream`);
 
-const cors = Cors({
-  methods: ['POST'],
-})
 
 const upload = multer({
   storage: multer.memoryStorage(),
@@ -32,8 +28,6 @@ function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
 
 
 const handler = async (req: any, res: any) => {
-  // Run the middleware
-  await runMiddleware(req, res, cors)
   await runMiddleware(req, res, upload.single(`file`))
 
   const dataStream = new stream.PassThrough()
