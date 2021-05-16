81b0fe91b140dd5544530e41274c17b587c3d975
ashwink0 <ashwin.kuppahally@gmail.com>
2021-05-15 20:32:32 -0700

    Switch to public download

--- src/pages/api/getFile/[id].ts
+++ src/pages/api/getFile/[id].ts
@@ -1,15 +1,11 @@
-import multer from 'multer';
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { v4 as uuid4 } from 'uuid';
+import { ObjectId } from 'bson';
 import { storage } from '../../../../lib/firebase-admin';
-const stream = require(`stream`);
 import { runMiddleware } from '../../../../middlewares/runMiddleware';
 import { connectToDatabase } from '../../../../middlewares/database';
-import { withAuth } from '../../../../middlewares/withAuth';
-import { rollbar } from '../../../../middlewares/rollbar';
 import { cors } from '../../../../middlewares/cors';
-import { ObjectId } from 'bson';
 import { FileDocument } from '../../../../utils/types';
+
+const stream = require(`stream`);
 
 const handler = async (req: any, res: any) => {
   await runMiddleware(req, res, cors);
@@ -19,9 +15,9 @@ const handler = async (req: any, res: any) => {
   } = req;
 
   const options = {
-    version: 'v2',
-    action: 'read',
-    expires: Date.now() + 1000 * 60,
+    version: `v2`,
+    action: `read`,
+    expires: Date.now() + 1000 * 10,
   };
 
   const uid = id.substring(0, 28);
@@ -37,22 +33,26 @@ const handler = async (req: any, res: any) => {
     });
     if (response) {
       const file = storage.bucket().file(response.firebaseStorageFileId);
-      let fileUrl: any = await file
+      let finalURL;
+      const fileUrl: any = await file
         .getSignedUrl({
-          version: 'v2',
-          action: 'read',
-          expires: Date.now() + 1000 * 60 * 10,  //5 min
+          version: `v2`,
+          action: `read`,
+          expires: Date.now() + 1000 * 60 * 10, // 5 min
+        })
+        .then((signedURLs) => {
+          finalURL = signedURLs[0];
         })
         .catch((err) => {
           console.log(err);
         });
 
-        res.redirect(fileUrl[0])
+      res.redirect(finalURL);
     } else {
-      res.send("This file does not exist").status(404);
+      res.send(`This file does not exist`).status(404);
     }
   } catch (err) {
-     res.send("This file does not exist").status(404);
+    res.send(`This file does not exist`).status(404);
   }
 };
 
