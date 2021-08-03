e059d174f3e7a81cbbc6f49aeded735d5fdd4062
ashwink0 <ashwin.kuppahally@gmail.com>
2021-08-02 21:12:48 -0700

    Fix landing and tile

--- src/components/Home/Images/ImageTile.tsx
+++ src/components/Home/Images/ImageTile.tsx
@@ -8,22 +8,13 @@ export default function ImageTile(props: { file: FileDocumentMongo }) {
 
   return (
     <figure className="m-2 w-72 md:w-96 md:flex transition duration-500 ease-in-out bg-gray-100 rounded-xl md:hover:shadow-lg m-4">
-      {/*<div className="flex flex-col justify-center items-center ">*/}
-      {/*  <img*/}
-      {/*    className="object-contain h-16 w-full md:rounded-none rounded-full mx-auto"*/}
-      {/*    alt="Logo"*/}
-      {/*    src="/firevault.png"*/}
-      {/*  />*/}
-      {/*</div>*/}
       <div className="w-full p-1 w-3/4 text-left m-0 md:p-6  text-center md:text-left space-y-4 break-all flex flex-col justify-between">
         <blockquote>
           <p className="text-lg font-semibold">{name}</p>
         </blockquote>
         <figcaption className="font-medium">
           <div className="text-cyan-600">{dateAdded}</div>
-          <a className="text-gray-500" href={link}>
-            Link
-          </a>
+          <a href={link}>Link</a>
         </figcaption>
       </div>
     </figure>
--- src/components/Home/Loading.tsx
+++ src/components/Home/Loading.tsx
@@ -1,5 +1,11 @@
-import { CircularProgress } from '@material-ui/core';
-
 export default function Loading() {
-  return <div>{/* <CircularProgress/> */}</div>;
+  return (
+    <div className="flex flex-col w-screen h-screen justify-center align-middle">
+      <img
+        className="object-contain h-28 w-full md:rounded-none rounded-full mx-auto"
+        alt="Logo"
+        src="/firevault.png"
+      />
+    </div>
+  );
 }
