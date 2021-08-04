f84fdcf682e4f0d104eb498100da6d5bc528fbbb
ashwink0 <ashwin.kuppahally@gmail.com>
2021-08-03 19:54:50 -0700

    Add pwa icons

--- src/components/PageHead.tsx
+++ src/components/PageHead.tsx
@@ -12,6 +12,14 @@ export default function PageHead() {
       <meta property="og:description" content="" />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:creator" content="@not_profitable" />
+
+      <link rel='manifest' href='/manifest.json' />
+      <link href='/icon-256x256.png' rel='icon' type='image/png' sizes='256x256' />
+      <link href='/icon-384x384.png' rel='icon' type='image/png' sizes='384x384' />
+      <link href='/icon-512x512.png' rel='icon' type='image/png' sizes='512x512' />
+
+      <link rel='apple-touch-icon' href='/icon-192x192.png'></link>
+      <meta name='theme-color' content='#2d3748' />
     </Head>
   );
 }
