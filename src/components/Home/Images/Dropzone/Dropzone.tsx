7a51d093baeb2a460c4478f2ff40a485c31dc991
ashwink0 <ashwin.kuppahally@gmail.com>
2021-08-14 18:09:20 -0700

    Update multiple file styling

--- src/components/Home/Images/Dropzone/Dropzone.tsx
+++ src/components/Home/Images/Dropzone/Dropzone.tsx
@@ -9,9 +9,10 @@ import {
   CircularProgress,
 } from '@material-ui/core';
 import Alert from '@/components/Alert';
-import fire from '../../../../../utils/firebase';
 import CheckBoxIcon from '@material-ui/icons/CheckBox';
 import ErrorIcon from '@material-ui/icons/Error';
+import fire from '../../../../../utils/firebase';
+
 const statusName = require(`http-status`);
 
 const getColor = (props: {
@@ -56,7 +57,7 @@ function DropzoneArea(props: { reloadData: Function }) {
   const [customName, setCustomName] = useState(``);
   let uploadError = false;
   const [fUSL, setFUSL] = useState(new Array(10));
-  let fileUploadStatusList = new Array(10);
+  const fileUploadStatusList = new Array(10);
   const clearStatuses = () => {
     fileUploadStatusList.map((item, index) => {
       fileUploadStatusList[index] = <></>;
@@ -76,7 +77,11 @@ function DropzoneArea(props: { reloadData: Function }) {
   } = useDropzone({ onDrop });
 
   const files = acceptedFiles.map((file, fIndex) => (
-    <div className="flex flex-row w-full text-left dark:bg-gray-500 justify-between items-center m-1 p-3">
+    <div
+      className={`flex flex-row w-full text-left dark:bg-gray-500 justify-between items-center m-1 p-3 ${
+        fIndex === 0 ? `rounded-tl-md rounded-tr-md` : ``
+      } ${fIndex === acceptedFiles.length-1 ? `rounded-bl-md rounded-br-md` : ``}`}
+    >
       <li key={file.name}>{file.name}</li>
       {fUSL[fIndex]}
     </div>
@@ -97,7 +102,7 @@ function DropzoneArea(props: { reloadData: Function }) {
     clearStatuses();
     setFUSL(fileUploadStatusList);
     acceptedFiles.map((item, index) => {
-      fileUploadStatusList[index] = <CircularProgress />;
+      fileUploadStatusList[index] = <CircularProgress size={30} />;
       setFUSL(fileUploadStatusList);
       setLoading(true);
       const formData = new FormData();
@@ -156,11 +161,7 @@ function DropzoneArea(props: { reloadData: Function }) {
       </div>
 
       <div className="flex flex-row justify-center align-middle p-2 m-3">
-        <Button
-          onClick={uploadFiles}
-          variant="contained"
-          color="primary"
-        >
+        <Button onClick={uploadFiles} variant="contained" color="primary">
           Upload
         </Button>
       </div>
