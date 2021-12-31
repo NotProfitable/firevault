ca8146a7d536885d7ca01418ad0eb925e53922bb
Ashwin K <ashwin.kuppahally@gmail.com>
2021-12-30 20:31:00 -0800

    Update Landing.tsx

--- src/components/Home/Landing.tsx
+++ src/components/Home/Landing.tsx
@@ -8,7 +8,12 @@ export default function Landing() {
       <LandingAppBar />
       <div className="flex flex-col items-center justify-center h-screen text-center text-black dark:text-white">
         <div className="m-32 min-w-max">
-          <h1 className="text-4xl font-bold">Landing</h1>
+          <img
+            className="object-contain h-28 w-full md:rounded-none rounded-full mx-auto"
+            alt="Logo"
+            src="/firevault.png"
+          />
+          <h1 className="text-4xl font-bold">Firevault</h1>
           <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={fire.auth()} className="min-w-max" />
         </div>
       </div>
